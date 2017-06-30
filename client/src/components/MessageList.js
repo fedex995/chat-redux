3036d3866c17dd0bdba6a89e10399e3e7850cf9f
federicotanzi <federictanzi@gamil.com>
2017-06-30 16:31:37 -0300

    messagelist.js

---
+++ client/src/components/MessageList.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class MessageList extends Component{
+
+    static propTypes = {
+        messages: PropTypes.array().isRequired
+    };
+
+    render() {
+
+        const {messages} = this.props;
+        let messageUi = messages.map(m => {
+            return (
+                <Message
+                    key={m.id}
+                    user={m.user}
+                    createdAt={m.createdAt}
+                    message={m.message}
+                />
+            )
+        });
+
+        return (
+            <div>
+                <h2>Messages</h2>
+                {messageUi}
+            </div>
+        )
+
+    }
+}
